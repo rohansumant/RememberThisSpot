bfc7bcf947fbec8b964be207560b3ea2f38fd5fc
Rohan Sumant <r.s.sumant@gmail.com>
2023-01-29 21:04:34 -0800

    1. Added react-native-geolocation-service package 2. Added location permissions in Android Manifest

--- App.tsx
+++ App.tsx
@@ -5,114 +5,58 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  Button,
+  PermissionsAndroid,
+  Text, View
 } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const addItem = () => {
+  console.log('Add item clicked');
+}
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const requestLocationPermission = async () => {
+  console.log(`requestLocationPermission called`);
+  try {
+    const result = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
+      title: 'Geolocation permission',
+      message: 'Can we access your location',
+      buttonPositive: 'Yes',
+      buttonNegative: 'No'
+    });
+    console.log(`location permission result: ${result}`);
+    return result === 'granted';
+  } catch (err) {
+    console.log(`Something went wrong: ${err}`);
+  }
+}
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+
+const getLocation = async (setLocation) => {
+  console.log(`getLocation invoked`);
+  const permissionGranted = await requestLocationPermission();
+  if (!permissionGranted) {
+    return;
+  }
+  Geolocation.getCurrentPosition(position => {
+    setLocation(position)
+  });
 }
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  const [location, setLocation] = useState(null);
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text>Latitude: {location?.coords.latitude}</Text>
+      <Text>Longitude: {location?.coords.longitude}</Text>
+      <Button
+        title="getLocation"
+        onPress={() => getLocation(setLocation)}/>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
