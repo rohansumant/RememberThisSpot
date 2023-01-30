2e4105e62850d28ec4e6c1ef9461a13f2fea36c5
Rohan Sumant <r.s.sumant@gmail.com>
2023-01-29 22:22:44 -0800

    1. Added a modal view to assign tag saved locations 2. Verified that deep-link to a lat/long location opens up in GMaps app.

--- App.tsx
+++ App.tsx
@@ -8,10 +8,13 @@
 import React, { useState } from 'react';
 import {
   Button,
+  Modal,
   PermissionsAndroid,
   Text, View
 } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
+import { styles } from './styles';
+import { Linking } from 'react-native';
 
 const addItem = () => {
   console.log('Add item clicked');
@@ -34,7 +37,6 @@ const requestLocationPermission = async () => {
   }
 }
 
-
 const getLocation = async (setLocation) => {
   console.log(`getLocation invoked`);
   const permissionGranted = await requestLocationPermission();
@@ -48,13 +50,28 @@ const getLocation = async (setLocation) => {
 
 function App(): JSX.Element {
   const [location, setLocation] = useState(null);
+  const [modalVisibility, setModalVisibility] = useState(false);
+
   return (
     <View>
       <Text>Latitude: {location?.coords.latitude}</Text>
       <Text>Longitude: {location?.coords.longitude}</Text>
       <Button
-        title="getLocation"
+        title="get Location"
         onPress={() => getLocation(setLocation)}/>
+      <Button
+        title="Home"
+        onPress={() => Linking.openURL(`https://maps.google.com/?q=${location.coords.latitude},${location.coords.longitude}`)} />
+      <Modal
+        transparent={false}
+        visible={modalVisibility}
+        style={styles.modalView}
+        onRequestClose={() => setModalVisibility(!modalVisibility)}>
+        <View><Text style={styles.textStyle}>Modal 1</Text></View>
+      </Modal>
+      <Button 
+        title='toggleModal'
+        onPress={() => setModalVisibility(!modalVisibility)} />
     </View>
   );
 }
